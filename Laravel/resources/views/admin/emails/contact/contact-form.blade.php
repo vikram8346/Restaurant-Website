4c764e74d9025c55950a64f09e9d798b7795eb1d
Vikram Vora <76270096+vikram8346@users.noreply.github.com>
2021-01-01 22:08:31 -0600

    Delete contact-form.blade.php

--- Laravel/resources/views/admin/emails/contact/contact-form.blade.php
+++
@@ -1,9 +0,0 @@
-@component('mail::message')
-
-# Thanks for the message
-<strong>Name</strong> {{$data['name']}}
-<strong>Email</strong> {{$data['email']}}
-<strong>Message</strong>
-
-{{$data['message']}}
-@endcomponent
